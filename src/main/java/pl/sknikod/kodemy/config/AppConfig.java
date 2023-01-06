cdcbdd9e2609d6e252a91b3221b6c8cb7285506e
Krystian Kielbasa <krystian.kielbasa@hotmail.com>
2023-01-06 21:00:50 +0100

    feat: #14 change config structure

---
+++ src/main/java/pl/sknikod/kodemy/config/AppConfig.java
@@ -0,0 +1,11 @@
+package pl.sknikod.kodemy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({
+        SecurityConfig.class
+})
+public class AppConfig {
+}
---
+++ src/main/java/pl/sknikod/kodemy/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package pl.sknikod.kodemy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();
+        http.authorizeRequests().anyRequest().permitAll();
+        http.csrf().disable();
+        return http.build();
+    }
+}
