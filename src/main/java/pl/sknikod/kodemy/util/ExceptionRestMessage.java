20fe73a77e2543dfa29bdc06a9f17709e1d0fb15
SpizganyTomek <oskarpiotrek80@gmail.com>
2023-02-28 16:30:57 +0100

    feat: #27 add ExceptionRestMessage

---
+++ src/main/java/pl/sknikod/kodemy/util/ExceptionRestMessage.java
@@ -0,0 +1,52 @@
+package pl.sknikod.kodemy.util;
+
+public class ExceptionRestMessage {
+
+    private long timeStamp;
+    private int status;
+    private String message;
+    private String description;
+
+    public ExceptionRestMessage() {
+
+    }
+
+    public ExceptionRestMessage(long timeStamp, int status, String message, String description) {
+        this.timeStamp = timeStamp;
+        this.status = status;
+        this.message = message;
+        this.description = description;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
