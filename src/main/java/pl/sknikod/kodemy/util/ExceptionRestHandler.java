7dca43fc10c6751ec6b54f46bf694da5217e2aae
SpizganyTomek <oskarpiotrek80@gmail.com>
2023-03-06 21:04:57 +0100

    fix: #27 constructors instead of setters in ExceptionRestHandler class, change value of description field to request.getDescription

--- src/main/java/pl/sknikod/kodemy/util/ExceptionRestHandler.java
+++ src/main/java/pl/sknikod/kodemy/util/ExceptionRestHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import pl.sknikod.kodemy.exception.ForbiddenCodeException;
 import pl.sknikod.kodemy.exception.NotFoundCodeException;
 import pl.sknikod.kodemy.exception.OAuth2AuthenticationProcessingException;
@@ -13,65 +14,51 @@ import pl.sknikod.kodemy.exception.UnauthorizedCodeException;
 public class ExceptionRestHandler {
 
     @ExceptionHandler
-    public ResponseEntity<ExceptionRestGenericMessage> handleException(Exception exc) {
-        ExceptionRestGenericMessage excMessage = new ExceptionRestGenericMessage();
-
-        excMessage.setStatus(HttpStatus.BAD_REQUEST.value());
-        excMessage.setMessage(exc.getMessage());
-        excMessage.setTimeStamp(System.currentTimeMillis());
-        excMessage.setDescription("Exception");
+    public ResponseEntity<ExceptionRestGenericMessage> handleException(Exception exc, WebRequest request) {
+        ExceptionRestGenericMessage excMessage = new ExceptionRestGenericMessage(System.currentTimeMillis(),
+                HttpStatus.BAD_REQUEST.value(),
+                exc.getMessage() != null ? exc.getMessage() : HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                request.getDescription(false));
 
         return new ResponseEntity<>(excMessage, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ExceptionRestGenericMessage> handleException(ForbiddenCodeException exc) {
-
-        ExceptionRestGenericMessage excMessage = new ExceptionRestGenericMessage();
-
-        excMessage.setStatus(HttpStatus.BAD_REQUEST.value());
-        excMessage.setMessage(exc.getMessage());
-        excMessage.setTimeStamp(System.currentTimeMillis());
-        excMessage.setDescription("Forbidden Exception");
+    public ResponseEntity<ExceptionRestGenericMessage> handleException(ForbiddenCodeException exc, WebRequest request) {
+        ExceptionRestGenericMessage excMessage = new ExceptionRestGenericMessage(System.currentTimeMillis(),
+                HttpStatus.FORBIDDEN.value(),
+                exc.getMessage() != null ? exc.getMessage() : HttpStatus.FORBIDDEN.getReasonPhrase(),
+                request.getDescription(false));
 
         return new ResponseEntity<>(excMessage, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ExceptionRestGenericMessage> handleException(NotFoundCodeException exc) {
-
-        ExceptionRestGenericMessage excMessage = new ExceptionRestGenericMessage();
-
-        excMessage.setStatus(HttpStatus.BAD_REQUEST.value());
-        excMessage.setMessage(exc.getMessage());
-        excMessage.setTimeStamp(System.currentTimeMillis());
-        excMessage.setDescription("Not Found Exception");
+    public ResponseEntity<ExceptionRestGenericMessage> handleNotFoundException(NotFoundCodeException exc, WebRequest request) {
+        ExceptionRestGenericMessage excMessage = new ExceptionRestGenericMessage(System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(),
+                exc.getMessage() != null ? exc.getMessage() : HttpStatus.NOT_FOUND.getReasonPhrase(),
+                request.getDescription(false));
 
         return new ResponseEntity<>(excMessage, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ExceptionRestGenericMessage> handleException(OAuth2AuthenticationProcessingException exc) {
-
-        ExceptionRestGenericMessage excMessage = new ExceptionRestGenericMessage();
-
-        excMessage.setStatus(HttpStatus.BAD_REQUEST.value());
-        excMessage.setMessage(exc.getMessage());
-        excMessage.setTimeStamp(System.currentTimeMillis());
-        excMessage.setDescription("OAuth2 Authentication Processing Exception");
+    public ResponseEntity<ExceptionRestGenericMessage> handleException(OAuth2AuthenticationProcessingException exc, WebRequest request) {
+        ExceptionRestGenericMessage excMessage = new ExceptionRestGenericMessage(System.currentTimeMillis(),
+                HttpStatus.BAD_REQUEST.value(),
+                exc.getMessage() != null ? exc.getMessage() : HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                request.getDescription(false));
 
         return new ResponseEntity<>(excMessage, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ExceptionRestGenericMessage> handleException(UnauthorizedCodeException exc) {
-
-        ExceptionRestGenericMessage excMessage = new ExceptionRestGenericMessage();
-
-        excMessage.setStatus(HttpStatus.BAD_REQUEST.value());
-        excMessage.setMessage(exc.getMessage());
-        excMessage.setTimeStamp(System.currentTimeMillis());
-        excMessage.setDescription("Unauthorized Exception");
+    public ResponseEntity<ExceptionRestGenericMessage> handleUnauthorizedException(UnauthorizedCodeException exc, WebRequest request) {
+        ExceptionRestGenericMessage excMessage = new ExceptionRestGenericMessage(System.currentTimeMillis(),
+                HttpStatus.UNAUTHORIZED.value(),
+                exc.getMessage() != null ? exc.getMessage() : HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                request.getDescription(false));
 
         return new ResponseEntity<>(excMessage, HttpStatus.UNAUTHORIZED);
     }
