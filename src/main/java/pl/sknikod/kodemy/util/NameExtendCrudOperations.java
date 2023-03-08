7dc6a4a0f906893dddf40c6d52b52f87495ba5d8
Krystian Kiełbasa <krystian.kielbasa@hotmail.com>
2023-03-08 02:27:30 +0100

    feat: #35 add Swagger responses, refactor **/me endpoint

--- src/main/java/pl/sknikod/kodemy/util/NameExtendCrudOperations.java
+++ src/main/java/pl/sknikod/kodemy/util/NameExtendCrudOperations.java
@@ -4,12 +4,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 public interface NameExtendCrudOperations<RES, REQ> extends BaseCrudOperations<RES, REQ> {
+    @BaseApiResponses.Read
     @GetMapping("/{name}")
-    public ResponseEntity<RES> getByName(@PathVariable String name);
+    ResponseEntity<RES> getByName(@PathVariable String name);
 
+    @BaseApiResponses.Delete
     @DeleteMapping("/{name}")
-    public void deleteByName(@PathVariable Long id);
+    void deleteByName(@PathVariable Long id);
 
+    @BaseApiResponses.Update
     @PutMapping("/{name}")
-    public ResponseEntity<RES> update(@PathVariable String name, @RequestBody REQ body);
+    ResponseEntity<RES> update(@PathVariable String name, @RequestBody REQ body);
 }
