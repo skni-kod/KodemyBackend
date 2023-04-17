882d6f2083b707388e5f120ee703fee61872dc6c
SpizganyTomek <oskarpiotrek80@gmail.com>
2023-04-17 18:14:59 +0200

    fix: #52 showGrades response limit to only few fields

--- src/main/java/pl/sknikod/kodemy/rest/response/MaterialShowGradesResponse.java
+++ src/main/java/pl/sknikod/kodemy/rest/response/MaterialShowGradesResponse.java
@@ -1,19 +1,52 @@
 package pl.sknikod.kodemy.rest.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.EqualsAndHashCode;
-import lombok.Value;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import pl.sknikod.kodemy.grade.Grade;
-import pl.sknikod.kodemy.material.MaterialStatus;
-import pl.sknikod.kodemy.rest.BaseDetails;
+import pl.sknikod.kodemy.user.User;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import java.util.Date;
-import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-@Value
+@Getter
+@Setter
+@NoArgsConstructor
 public class MaterialShowGradesResponse {
-    Set<Grade> grades;
+    private Set<GradeResponse> grades;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class GradeResponse {
+        private Long id;
+        private Double grade;
+        private UserResponse createdBy;
+
+        public GradeResponse(Long id, Double grade, User createdBy) {
+            this.id = id;
+            this.grade = grade;
+            this.createdBy = new UserResponse(createdBy.getId(), createdBy.getUsername());
+        }
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class UserResponse {
+        private Long id;
+        private String name;
+
+        public UserResponse(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    public MaterialShowGradesResponse(Set<Grade> grades) {
+        this.grades = grades.stream().map(grade -> {
+            GradeResponse gradeResponse = new GradeResponse(grade.getId(), grade.getGrade(), grade.getUser());
+            return gradeResponse;
+        }).collect(Collectors.toSet());
+    }
 }
