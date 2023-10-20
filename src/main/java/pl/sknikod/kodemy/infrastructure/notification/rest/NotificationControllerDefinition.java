18e599052924075b4dd13331b26f365d526b9250
Krystian Kiełbasa <kkielbasa@opgksoftware.com>
2023-10-20 10:41:53 +0200

    refactor: small changes

--- src/main/java/pl/sknikod/kodemy/infrastructure/notification/rest/NotificationControllerDefinition.java
+++ src/main/java/pl/sknikod/kodemy/infrastructure/notification/rest/NotificationControllerDefinition.java
@@ -1,5 +1,6 @@
 package pl.sknikod.kodemy.infrastructure.notification.rest;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import java.util.List;
 @SwaggerResponse.SuccessCode200
 @SwaggerResponse.UnauthorizedCode401
 @SwaggerResponse.ForbiddenCode403
+@Hidden
 public interface NotificationControllerDefinition {
     @GetMapping("/{notificationId}")
     @Operation(summary = "Show Notification by ID")
