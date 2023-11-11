590ce823203a3361e81a51d51f2174892f34ea76
Krystian Kiełbasa <krystian.kielbasa@hotmail.com>
2023-11-11 16:22:55 +0100

    refactor: #138 small changes

--- kodemy-backend/src/main/java/pl/sknikod/kodemybackend/infrastructure/common/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.sknikod.kodemybackend.infrastructure.common.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.sknikod.kodemybackend.infrastructure.common.entity.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
--- kodemy-backend/src/main/java/pl/sknikod/kodemybackend/infrastructure/material/MaterialUpdateUseCase.java
+++ kodemy-backend/src/main/java/pl/sknikod/kodemybackend/infrastructure/material/MaterialUpdateUseCase.java
@@ -37,7 +37,6 @@ public class MaterialUpdateUseCase {
     private final AuthService authService;
 
     public MaterialUpdateResponse execute(Long materialId, MaterialUpdateRequest body) {
-        // TODO refactor
         Material existingMaterial = entityDao.findMaterialById(materialId);
         return Option.of(body)
                 .map(materialUpdateRequest -> updateMaterialMapper.map(
