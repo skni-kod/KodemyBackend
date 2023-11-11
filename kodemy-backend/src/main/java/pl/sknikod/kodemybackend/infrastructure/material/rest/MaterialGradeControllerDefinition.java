5f687917b1904f748996d43448043acdf32e7c1f
marcinbator <marcinbator.ofc@gmail.com>
2023-11-11 11:40:25 +0100

    refactor: #136 grades search approach

--- kodemy-backend/src/main/java/pl/sknikod/kodemybackend/infrastructure/material/rest/MaterialGradeControllerDefinition.java
+++ kodemy-backend/src/main/java/pl/sknikod/kodemybackend/infrastructure/material/rest/MaterialGradeControllerDefinition.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,14 +32,11 @@ public interface MaterialGradeControllerDefinition {
     @SwaggerResponse.NotFoundCode404
     @GetMapping("/{materialId}/grades")
     ResponseEntity<Page<SingleGradeResponse>> showGrades(
-            @PathVariable Long materialId,
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-            @Parameter(description = "Format: yyyy-MM-ddTHH:mm:ss")
-            @RequestParam(value = "from") Date from,
-            @Parameter(description = "Format: yyyy-MM-ddTHH:mm:ss")
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-            @RequestParam(value = "to") Date to,
             @RequestParam(defaultValue = "20") int size,
-            @RequestParam(defaultValue = "0") int page
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(value = "sort", defaultValue = "createdDate") String sort,
+            @RequestParam(value = "sort_direction", defaultValue = "DESC") Sort.Direction sortDirection,
+            @Parameter(description = "{\"materialId\":number, \"createdDateFrom\":\"2023-01-01T00:00:00\",\"createdDateTo\":\"2023-01-01T00:00:00\"}")
+            @RequestParam(value = "search_fields", required = false) SearchFields searchFields
     );
 }
