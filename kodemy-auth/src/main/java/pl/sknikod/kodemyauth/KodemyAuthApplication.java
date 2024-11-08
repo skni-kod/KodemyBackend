7bcb46e7ca1dafcbbd90d9f26aa74ab7170fecbc
Marcin Bator <marcinbator.ofc@gmail.com>
2024-11-08 21:02:28 +0100

    feat: #182 service registry, gateway

--- kodemy-auth/src/main/java/pl/sknikod/kodemyauth/KodemyAuthApplication.java
+++ kodemy-auth/src/main/java/pl/sknikod/kodemyauth/KodemyAuthApplication.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -28,6 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 bearerFormat = "JWT"
         )
 })
+@EnableDiscoveryClient
 public class KodemyAuthApplication {
 
     public static void main(String[] args) {
