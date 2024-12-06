cd2c4b0ac19b936c8763466278fef769b16ecf81
Marcin Bator <marcinbator.ofc@gmail.com>
2024-12-06 09:22:21 +0100

    feat: #185 search service test setup

---
+++ kodemy-search/src/test/groovy/TSpec.groovy
@@ -0,0 +1,14 @@
+import spock.lang.Specification
+
+class TSpec extends Specification {
+    def "shouldTrue"() {
+        given:
+            def email = ""
+        when:
+            email = ""  
+        then:
+            verifyAll {
+                email == ""
+            }
+    }
+}
