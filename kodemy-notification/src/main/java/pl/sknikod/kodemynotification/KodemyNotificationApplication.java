f11a6895209836574b2e05c06a10db204d37b947
Krystian Kiełbasa <kkielbasa@opgksoftware.com>
2024-12-26 22:46:36 +0100

    feat: #202 discovery, notification test init

--- kodemy-notification/src/main/java/pl/sknikod/kodemynotification/KodemyNotificationApplication.java
+++ kodemy-notification/src/main/java/pl/sknikod/kodemynotification/KodemyNotificationApplication.java
@@ -1,25 +1,22 @@
 package pl.sknikod.kodemynotification;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(
-        title = "KodemyBackend API - kodemy-backend",
-        version = "",
-        description = "SKNI Kod Kodemy"
-), security = @SecurityRequirement(name = "Bearer Authentication"))
-@SecurityScheme(
-        name = "Bearer Authentication",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
+@OpenAPIDefinition(
+        info = @Info(
+                title = "KodemyBackend API - kodemy-auth",
+                version = "",
+                description = "SKNI Kod Kodemy"
+        ),
+        security = {
+                @SecurityRequirement(name = "authorization_bearer")
+        }
 )
 @EnableDiscoveryClient
 public class KodemyNotificationApplication {
